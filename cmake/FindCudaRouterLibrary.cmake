32b11cbed0f1a3b0df35051370ded81096bd560a
sarunassarakojis <sarakojis.sarunas9@gmail.com>
2018-04-21 19:13:00 +0300

    Include and link cuda router lib

---
+++ cmake/FindCudaRouterLibrary.cmake
@@ -0,0 +1,21 @@
+# Attempts to find CudaRouterLibrary which should normally be installed 
+# in corresponding folder (x64) of program files on Windows platform.
+
+# Once done this will define:
+#  CudaRouterLibrary_FOUND          - library has been located
+#  CudaRouterLibrary_INCLUDE_DIRS   - the CudaRouterLibrary include directories
+#  CudaRouterLibrary_LIBRARIES      - The libraries needed to use CudaRouterLibrary (Release)
+#  CudaRouterLibrary_LIBRARIES_DEB  - The libraries needed to use CudaRouterLibrary (Debug)
+
+set (CudaRouterLibrary_FOUND FALSE)
+
+if (WIN32)
+    set (CUDA_ROUTER_LIBRARY_HOME "$ENV{PROGRAMFILES}/cuda-vehicle-router")
+    set (CMAKE_PREFIX_PATH ${CMAKE_PREFIX_PATH} ${CUDA_ROUTER_LIBRARY_HOME})
+
+    find_path (CudaRouterLibrary_INCLUDE_DIRS NAMES router_library.h)
+    find_library (CudaRouterLibrary_LIBRARIES NAMES cuda-vehicle-router-lib)
+    find_library (CudaRouterLibrary_LIBRARIES_DEB NAMES cuda-vehicle-router-libd PATH_SUFFIXES debug/lib)
+
+    set (CudaRouterLibrary_FOUND TRUE)
+endif ()
